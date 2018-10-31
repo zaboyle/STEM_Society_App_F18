3d69528ccc641246c3f2e1988c132dc30161b8b7
Zach Boyle <zboyle@zachs-imac.local>
2018-10-31 19:07:23 -0400

    Initial Commit

---
+++ STEM_Society_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STEM_Society_App
+//
+//  Created by Zach Boyle on 10/31/18.
+//  Copyright © 2018 Zach Boyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
